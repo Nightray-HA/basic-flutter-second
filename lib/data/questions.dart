894e68256475a60b93790f6cc59821b23b39885b
Hilal Abdullah <hilal.spentisa99@gmail.com>
2024-03-18 07:48:48 +0700

    widget component

---
+++ lib/data/questions.dart
@@ -0,0 +1,24 @@
+import 'package:second_basic/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion('What are the main building blocks of  Flutter UIs?',
+      ['Widgets', 'Blocks', 'Bricks', 'Pieces']),
+  QuizQuestion('What is the name of the class that is the entry point for all Flutter apps?',
+      ['App', 'Main', 'Start', 'MyApp']),
+  QuizQuestion('What method is called when the widget is inserted into the widget tree?',
+      ['initState', 'build', 'create', 'onStart']),
+  QuizQuestion('What property is used to create responsive UIs in Flutter?',
+      ['Expanded', 'Flexible', 'Layout', 'Responsive']),
+  QuizQuestion('What is the name of the app that is created when you create a new Flutter project?',
+      ['MyApp', 'FlutterApp', 'App', 'MainApp']),
+  QuizQuestion('What is the name of the method that is called when the widget is removed from the widget tree?',
+      ['dispose', 'remove', 'onRemove', 'onDispose']),
+  QuizQuestion('What is the name of the method that is called when the widget is updated?',
+      ['didUpdateWidget', 'update', 'onUpdate', 'onWidgetUpdate']),
+  QuizQuestion('What is the name of the method that is called when the widget is removed from the widget tree?',
+      ['dispose', 'remove', 'onRemove', 'onDispose']),
+  QuizQuestion('What is the name of the method that is called when the widget is updated?',
+      ['didUpdateWidget', 'update', 'onUpdate', 'onWidgetUpdate']),
+  QuizQuestion('What is the name of the method that is called when the widget is removed from the widget tree?',
+      ['dispose', 'remove', 'onRemove', 'onDispose']),
+];
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:second_basic/answer_button.dart';
+import 'package:second_basic/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
-  
+
   @override
   State<QuestionScreen> createState() {
     return _QuestionScreenState();
@@ -12,6 +14,25 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return const Text('Question Screen');
+    final currentQuestions = questions;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question goes here...',
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+              )),
+          const SizedBox(height: 30),
+          AnswerButton(
+            answerText: 'Answer 1',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
